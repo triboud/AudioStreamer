a3c71188cb9774d25c8d72c1191a29eaf42b98d8
Andrew Armstrong <phplasma@gmail.com>
2012-05-09 23:18:08 +1000

    Added Blocks file that defines the SimpleBlock class

---
+++ Classes/Blocks.h
@@ -0,0 +1,9 @@
+//
+//  Blocks.h
+//  Player
+//
+//  Created by Andrew Armstrong on 9/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+typedef void (^SimpleBlock)(void);
